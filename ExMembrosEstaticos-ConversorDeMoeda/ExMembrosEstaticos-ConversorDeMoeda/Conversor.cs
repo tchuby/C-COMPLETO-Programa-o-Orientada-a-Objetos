924bfa983f09f68bcab100e8f2e88d45c709553c
Daniel Andrade Silva <daniel.andrade@ambevtech.com.br>
2021-01-22 08:48:32 -0300

    Alteração de acesso da classe Conversor.

--- ExMembrosEstaticos-ConversorDeMoeda/ExMembrosEstaticos-ConversorDeMoeda/Conversor.cs
+++ ExMembrosEstaticos-ConversorDeMoeda/ExMembrosEstaticos-ConversorDeMoeda/Conversor.cs
@@ -1,13 +1,12 @@
 ﻿namespace ExMembrosEstaticos_ConversorDeMoeda
 {
-    public class Conversor
+    public static class Conversor
     {
-        public static double Iof = 1.06;
+        private static double Iof = 1.06; 
 
         public static double Converter(double cotacao, double qtd)
         {
             return cotacao * qtd * Iof;
         }
     }
-
 }
--- ExMembrosEstaticos-ConversorDeMoeda/ExMembrosEstaticos-ConversorDeMoeda/Program.cs
+++ ExMembrosEstaticos-ConversorDeMoeda/ExMembrosEstaticos-ConversorDeMoeda/Program.cs
@@ -3,15 +3,20 @@ using System.Globalization;
 
 namespace ExMembrosEstaticos_ConversorDeMoeda
 {
-    class Program : Conversor
+    static class Program
     {
         static void Main(string[] args)
         {
+            Converte();
+        }
+
+        static void Converte()
+        {
             Console.Write("Qual é a cotação do dólar? ");
-            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             Console.Write("Quantos dólares você vai comparar? ");
             double qtd = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            var reais = Converter(cotacao, qtd);
+            var reais = Conversor.Converter(cotacao, qtd);
             Console.WriteLine($"Valor a ser pago em reais = {reais.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
