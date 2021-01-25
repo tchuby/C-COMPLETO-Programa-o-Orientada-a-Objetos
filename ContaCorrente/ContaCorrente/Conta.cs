c506951d50bd2e8eb7cc1729725b87414d7d1145
Daniel Andrade Silva <daniel.andrade@ambevtech.com.br>
2021-01-25 13:31:57 -0300

    Criados os campos da Classe Conta, ainda não finalizado.

---
+++ ContaCorrente/ContaCorrente/Conta.cs
@@ -0,0 +1,13 @@
+﻿namespace ContaCorrente
+{
+    public class Conta
+    {
+        private const double _taxa = 5.00;
+        private string _saldo;
+
+        public int IdConta { get; protected set; }
+        public string NomeTitular { get; protected set; }
+
+
+    }
+}
---
+++ ContaCorrente/ContaCorrente/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContaCorrente
+{
+    class Program
+    {
+        private static NumberStyles cultureInfo;
+
+        static void Main(string[] args)
+        {
+            Operar();
+            Console.ReadKey();
+        }
+        
+        static void Operar()
+        {
+            string numeroConta = "Entre o número da conta:";
+            string entrarNome = "Entre o titular da conta:";
+            string condicaoDepositoInicial = "Havera depósito inicial (s/n)?";
+            string entrarDepositoInicial = "Entre o valor de depósito inicial:";
+
+            Console.Write(numeroConta);
+            int nConta = int.Parse(Console.ReadLine());
+
+            Console.Write(entrarNome);
+            string nomeTitular = Console.ReadLine();
+
+            Console.Write(condicaoDepositoInicial);
+            string condDepositoInicial = Console.ReadLine();
+
+            if (condDepositoInicial == "s")
+            {
+                Console.Write(entrarDepositoInicial);
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }                
+        }
+    }
+}
