4295453a7f040f51c5b8ba14c3c379f51333a4f4
Daniel Andrade Silva <daniel.andrade@ambevtech.com.br>
2021-02-05 16:01:22 -0300

    Solução Exercitando listas

---
+++ ExercitandoListas/ExercitandoListas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExercitandoListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+            list.Insert(2, "Marco"); //o insert adiciona na posição definida
+
+            foreach( var obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.WriteLine("List count: " + list.Count);
+        }
+    }
+}
