db6474f291f5f153e3ab3c8dba005454dd58364a
daniel.tchuby@outlook.com <daniel.tchuby@outlook.com>
2021-04-05 21:18:40 -0300

    Exercício Matrix1

---
+++ ExercitandoMatrizes/Matrix1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Matrix1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OnStart();
+        }
+
+        public static void OnStart()
+        {
+            var matrix = InputMatrix();
+            PrintMainDaigonal(matrix);
+            PrintCountNegativeValues(matrix);
+        }
+
+        public static int[,] InputMatrix()
+        {
+            int matrixOrder = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[matrixOrder, matrixOrder];
+
+            for(int i = 0; i < matrixOrder; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for(int j = 0; j < matrixOrder; j++)
+                {
+                    matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void PrintMainDaigonal(int[,] matrix)
+        {
+            Console.WriteLine("Main diagonal:");
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintCountNegativeValues(int[,] matrix)
+        {
+            int count = 0;
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0)
+                        count++;
+                }
+            }
+            Console.WriteLine($"Negative numbers = {count}");
+        }
+    }
+}
