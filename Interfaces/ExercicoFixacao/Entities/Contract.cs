28bc1a8ddb592192c304b926a308682f3eb27836
Daniel Tchuby <danielandradesilva.84@gmail.com>
2021-09-05 16:45:24 -0300

    Arquivos do exercícios de Interface e Conjuntos.

---
+++ Interfaces/ExercicoFixacao/Entities/Contract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExercicoFixacao.Entities
+{
+    public class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public Installment Installment { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+        }
+    }
+}
